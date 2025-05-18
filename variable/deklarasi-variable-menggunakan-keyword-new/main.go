d1c52826cd0f73eef2ef77273bd13c19a27958ad
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-05-18 11:05:51 +0700

    menambahkan variable user dengan struct

--- variable/deklarasi-variable-menggunakan-keyword-new/main.go
+++ variable/deklarasi-variable-menggunakan-keyword-new/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name string
+}
+
 func main() {
 	name := new(string) // variable name menampung data bertipe pointer string
 
@@ -12,4 +16,13 @@ func main() {
 	// untuk memberi nilainya dengan cara dereference
 	*name = "Yusuf Eko Anggoro"
 	fmt.Println(*name)
+
+	// dengan struct
+	user := new(User)
+
+	(*user).Name = "Yusuf Eko Anggoro Manual" // dengan pointer dereferencing manual
+	fmt.Printf("Hi! My Name %s %s", user.Name, "\n")
+
+	user.Name = "Yusuf Eko Anggoro Auto" // dengan pointer dereferencing otomatis untuk field dan method
+	fmt.Printf("Hi! My Name %s", user.Name)
 }
