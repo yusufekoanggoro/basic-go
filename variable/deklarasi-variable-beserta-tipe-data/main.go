71f233c4ae72ce14dc2392749974a14d7aa34f88
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-05-18 10:06:53 +0700

    deklarasi variable beserta tipe data

---
+++ variable/deklarasi-variable-beserta-tipe-data/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var <nama-variable> <tipe data> = <nilai>
+	var firstName string = "Yusuf"
+	var lastName string
+
+	lastName = "Eko Anggoro"
+
+	fmt.Printf("Hi! My Name %s %s", firstName, lastName)
+}
